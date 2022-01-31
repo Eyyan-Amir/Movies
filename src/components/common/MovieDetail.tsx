66b4c2f6fa1cf34dfeb4efc3457794c531f8305b
ayyan <ayyanamir6@gmail.com>
2022-01-31 12:17:54 +0500

    movies detail page back btn add

--- src/components/common/MovieDetail.tsx
+++ src/components/common/MovieDetail.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import * as yup from "yup";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import CommentsForm from "./CommentsForm";
 import { useSelector, useDispatch } from "react-redux";
 import { setMovieDetail, addComments } from "../../action/action";
@@ -57,49 +58,54 @@ export default function MovieDetail() {
 	}, [items]);
 
 	return (
-		<div className="movie-detail">
-			<div className="d-flex">
-				<div className="image my-3">
-					<img
-						src={`${process.env.REACT_APP_MOVIES_IMG}${detailMovie.backdrop_path}`}
-						alt=""
-						className="img-fluid h-100"
-					/>
-				</div>
-				<div className="detail my-3">
-					<p>
-						<span className="bold">OverView :</span> {detailMovie.overview}
-					</p>
-					<p>
-						<span className="bold">popularity :</span> {detailMovie.popularity}
-					</p>
-					<p>
-						<span className="bold">Budget :</span> {detailMovie.budget}
-					</p>
-					{detailMovie.homepage && (
-						<p>
-							<span className="bold">HomePage :</span>{" "}
-							<a href={detailMovie.homepage}>{detailMovie.homepage}</a>
-						</p>
-					)}
-				</div>
-			</div>
-			<div className="title">{detailMovie.title}</div>
+		<>
+			<Link to={"/home"}>back</Link>
 
-			<CommentsForm
-				initialValue={initialValue}
-				handleSubmit={handleSubmit}
-				validationSchema={validationSchema}
-				type="text"
-				control="textarea"
-				name="comment"
-				placeHolder="comments"
-			/>
-			<ul>
-				{items.map((item: any, i: number) => {
-					return <li key={i}>{item.comment}</li>;
-				})}
-			</ul>
-		</div>
+			<div className="movie-detail">
+				<div className="d-flex">
+					<div className="image my-3">
+						<img
+							src={`${process.env.REACT_APP_MOVIES_IMG}${detailMovie.backdrop_path}`}
+							alt=""
+							className="img-fluid h-100"
+						/>
+					</div>
+					<div className="detail my-3">
+						<p>
+							<span className="bold">OverView :</span> {detailMovie.overview}
+						</p>
+						<p>
+							<span className="bold">popularity :</span>{" "}
+							{detailMovie.popularity}
+						</p>
+						<p>
+							<span className="bold">Budget :</span> {detailMovie.budget}
+						</p>
+						{detailMovie.homepage && (
+							<p>
+								<span className="bold">HomePage :</span>{" "}
+								<a href={detailMovie.homepage}>{detailMovie.homepage}</a>
+							</p>
+						)}
+					</div>
+				</div>
+				<div className="title">{detailMovie.title}</div>
+
+				<CommentsForm
+					initialValue={initialValue}
+					handleSubmit={handleSubmit}
+					validationSchema={validationSchema}
+					type="text"
+					control="textarea"
+					name="comment"
+					placeHolder="comments"
+				/>
+				<ul>
+					{items.map((item: any, i: number) => {
+						return <li key={i}>{item.comment}</li>;
+					})}
+				</ul>
+			</div>
+		</>
 	);
 }
